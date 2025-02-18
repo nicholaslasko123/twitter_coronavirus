d34e46fd95fc4ad08e4d4129ac9a932048a45eb6
nicholaslasko123 <nlaskowski25@students.claremontmckenna.edu>
2025-02-17 17:18:08 -0800

    Update run_maps.sh: loop over files with nohup and run in parallel

--- run_maps.sh
+++ run_maps.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-# Define the directory containing the dataset
-DATASET_DIR="/data/Twitter dataset"
-
-# Loop over each matching file in the dataset directory
-for file in "$DATASET_DIR"/geoTwitter20-*-*.zip; do
-  echo "Launching process for $file"
-  nohup python3 src/map.py --input_path="$file" > "${file}_nohup.log" 2>&1 &
+for file in "/data/Twitter dataset"/geoTwitter20-*-*.zip; do
+    echo "Processing $file"
+    nohup python3 ./src/map.py --input_path="$file" > "${file}_nohup.log" 2>&1 &
 done
-
-echo "All map.py processes have been started."
 
